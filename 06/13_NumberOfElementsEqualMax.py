36c1fed3c43f11d2ab8cd38f09755e0697c7ee39
irk2adm <irk2adm@gmail.com>
2018-03-12 23:11:26 +0300

    L6 + 13_NumberOfElementsEqualMax.py (Количество элементов, равных максимуму).

---
+++ 06/13_NumberOfElementsEqualMax.py
@@ -0,0 +1,13 @@
+# Задача «Количество элементов, равных максимуму»
+# Последовательность состоит из натуральных чисел и завершается числом 0. Определите, сколько элементов этой последовательности равны ее наибольшему элементу. 
+
+maximum = 0
+num_maximal = 0
+element = -1
+while element != 0:
+    element = int(input())
+    if element > maximum:
+        maximum, num_maximal = element, 1
+    elif element == maximum:
+        num_maximal += 1       
+print(num_maximal)
