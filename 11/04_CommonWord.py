d971237da62a461d15bf5ef6059d604483c2a894
irk2adm <irk2adm@gmail.com>
2018-03-19 14:50:58 +0300

    L11 + 11/04_CommonWord.py (Самое частое слово).

---
+++ 11/04_CommonWord.py
@@ -0,0 +1,14 @@
+Задача «Самое частое слово»
+Условие
+
+Дан текст: в первой строке задано число строк, далее идут сами строки. Выведите слово, которое в этом тексте встречается чаще всего. Если таких слов несколько, выведите то, которое меньше в лексикографическом порядке.
+
+counter = {}
+for i in range(int(input())):
+    line = input().split()
+    for word in line:
+        counter[word] = counter.get(word, 0) + 1
+         
+max_count = max(counter.values())
+most_frequent = [k for k, v in counter.items() if v == max_count]
+print(min(most_frequent))
