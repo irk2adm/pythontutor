c9317ee3f3f67d9a084284fcf74aa0d843da8d6b
irk2adm <irk2adm@gmail.com>
2018-03-19 12:09:08 +0300

    L9. 6 out of 6. + 09/06_ChangeColumns.py (Поменять столбцы).

---
+++ 09/06_ChangeColumns.py
@@ -0,0 +1,14 @@
+# Задача «Поменять столбцы»
+# Дан двумерный массив и два числа: i и j. Поменяйте в массиве столбцы с номерами i и j и выведите результат.
+# Программа получает на вход размеры массива n и m, затем элементы массива, затем числа i и j.
+# Решение оформите в виде функции swap_columns(a, i, j).
+
+def swap_columns(a, i, j):
+    for k in range(len(a)):
+        a[k][i], a[k][j] = a[k][j], a[k][i]
+ 
+n, m = [int(i) for i in input().split()]
+a = [[int(j) for j in input().split()] for i in range(n)]
+i, j = [int(i) for i in input().split()]
+swap_columns(a, i, j)
+print('\n'.join([' '.join([str(i) for i in row]) for row in a]))
